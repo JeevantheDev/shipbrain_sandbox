1ded683550a1126c8c5338faadd369b2b8c8cdfc
Jeevan Jyoti Dash <dashjeevanjyoti04@gmail.com>
2026-05-27 15:20:14 +0530

    chore: clean up debug variables from release endpoint

--- functions/api/release.js
+++ functions/api/release.js
@@ -4,10 +4,7 @@ export async function onRequestGet(context) {
   return new Response(JSON.stringify({
     releaseVersion,
     repo: "JeevantheDev/shipbrain_sandbox",
-    mode: "mock-cart-checkout",
-    envKeys: Object.keys(env),
-    apiUrl: env.SHIPBRAIN_API_URL ?? null,
-    webhookUrl: env.SHIPBRAIN_INCIDENT_WEBHOOK_URL ?? null
+    mode: "mock-cart-checkout"
   }, null, 2), {
     status: 200,
     headers: { "content-type": "application/json" }
